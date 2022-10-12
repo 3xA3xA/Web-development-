8631be275e77cfc9f085121875f838c201a4c84d
3xA3xA <drainedsimp@gmail.com>
2022-10-12 20:41:59 +0300

    Commit 1

---
+++ lw11/src/getSurveys.php
@@ -0,0 +1,33 @@
+<?php
+function getFileSubstring(?string $fileString): ?string
+{
+    return (trim(substr($fileString, strpos($fileString, ':') + 1)));
+}
+
+if (!file_exists("./data"))
+{
+    mkdir("./data");
+}
+
+$arrayFile = scandir("./data");
+array_shift($arrayFile);
+array_shift($arrayFile); 
+$out =[];
+
+foreach($arrayFile as $path) {
+    $file = fopen("./data/" . $path, "r");
+    $name = getFileSubstring(fgets($file));
+    $email = getFileSubstring(fgets($file));
+    $activity = getFileSubstring(fgets($file));
+    $agreement = getFileSubstring(fgets($file));
+    fclose($file);
+
+    $data = array(
+        "name" => $name,
+        "email" => $email,
+        "activity" => $activity,
+        "agreement" => $agreement
+    );
+    array_push($out, $data);
+}
+echo json_encode($out, JSON_FORCE_OBJECT);
